e0512977b902c88f7706dda4605a3a1685347350
Jairik <jairikmc@gmail.com>
2024-03-01 23:38:29 -0500

    Added small sound functionality
    Will need to be finalized at the end of the project when all relevant files are in. Just a small addition that will be helpful later

--- battleship/src/Project1/battleshipController.java
+++ battleship/src/Project1/battleshipController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.imageio.ImageIO;
+import javax.sound.sampled.*;
+import java.io.File;
 import javax.swing.*;
 
 
@@ -16,6 +18,7 @@ public class battleshipController implements ActionListener{
     public battleshipController() throws IOException {
         view = new battleshipView();
         model = new battleshipModel();
+        //playGameMusic();
         fireCannon();
         
         
@@ -39,6 +42,12 @@ public void actionPerformed(ActionEvent e) {
         //if statement calls checkforvalidshot() from model if true hit = X else miss = O
         if(model.checkForValidShot(position[0], position[1])){
             HitOrMiss = "X";
+            if(HitOrMiss == "X") {
+                //playHitSound();
+            }
+            else {
+                //playMissSound();
+            }
             view.updateView(position[0], position[1], HitOrMiss);
             model.updateModel(position[0], position[1], HitOrMiss); // updates the model
             if(model.shipStatus()){ // checks if ship sank. 
@@ -50,6 +59,55 @@ public void actionPerformed(ActionEvent e) {
         }
         //updates board accordingly in view
         view.updateView(position[0], position[1], HitOrMiss); 
-    }    
+    } 
+    
+    /*--Sound-Effects--
+    //There is a way we can package this all up but its likely easier if we get all of our images and relevent
+    //Files first. For Testing, we can leave this commented out
+    //When called, will play an water splash sound effect for misses
+    void playMissSound() {
+        try {
+            String filename = "C:\\Users\\jairi\\OneDrive\\Desktop\\COSC330-Battleship-\\COSC330-Battleship-\\battleship\\resources\\Big Water Splash Sound Effect.wav";
+            File missSoundEffect = new File(filename);
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(missSoundEffect);
+            Clip missSoundClip = AudioSystem.getClip();
+            missSoundClip.open(audioIn);
+            missSoundClip.start();
+        }
+        catch (Exception e) {
+            //Can do whatever we just need a try catch statement
+        }
+    }
+
+    //When called, will play an explosion sound effect for hits
+    void playHitSound() {
+        try {
+            String filename = "C:\\Users\\jairi\\OneDrive\\Desktop\\COSC330-Battleship-\\COSC330-Battleship-\\battleship\\resources\\EXPLOSION SOUND EFFECT (No Copyright).wav";
+            File hitSoundEffect = new File(filename);
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(hitSoundEffect);
+            Clip hitSoundClip = AudioSystem.getClip();
+            hitSoundClip.open(audioIn);
+            hitSoundClip.start();
+        }
+        catch (Exception e) {
+            //Can do whatever we just need a try catch statement
+        }
+    }
+
+    //When called, will start the game music and will loop indefinitely
+    void playGameMusic() {
+        try {
+            String filename = ""; //Can be whatever music, just must be converted to WAV file first
+            File gameMusic = new File(filename);
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(gameMusic);
+            Clip gameMusicClip = AudioSystem.getClip();
+            gameMusicClip.open(audioIn);
+            gameMusicClip.loop(Clip.LOOP_CONTINUOUSLY); //Will continously loop the song when it ends
+            gameMusicClip.start();
+        }
+        catch (Exception e) {
+            //Can do whatever we just need a try catch statement
+        }
+    } */
 }
 
